2c72e3839918dbdbde2ca746899df32dc5353543
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-11 11:49:03 +0300

    Done Task00 and Task01

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Square
+Console.WriteLine("Введите целое число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+// string strNum = Console.Readline (); 
+// int number = Convert.ToInt32(strNum);
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+// Console.Write("Квадрат числа равен ");
+// Console.WriteLine(square);
+
+// if(number ==1) Console.WriteLine("Понедельник");
+// else if(number ==2) Console.WriteLine("Вторник");
+// else Console.WriteLine("Число неверно");
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Проверка квадрата
+Console.WriteLine("Нужно ввести два целых числа");
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberB ==numberA * numberA)
+{
+    Console.WriteLine($"Число {numberB} является квадратом {numberA}");
+}
+else {
+    Console.WriteLine($"Число {numberB} не является квадратом {numberA}");
+}
