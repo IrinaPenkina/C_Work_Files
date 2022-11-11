aae444dad61c8db47d99fd4bb0eb264d63c47587
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-11 12:56:26 +0300

    Added Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+}
+else Console.WriteLine("Введено неверное число");
