e5f6f8be60e5715d2e270816318e02da9ab92dae
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-15 11:17:45 +0300

    task 005 done

---
+++ Task005/Program.cs
@@ -0,0 +1,17 @@
+﻿// выводит случайное число из отрезка [100, 999]
+// и показывает двузнчное число из первой и последней цифр
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 --> {number}");
+
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit  = num % 10;
+    return firstDigit * 10 + thirdDigit;
+}
+
+int resultDigit = DeleteSecondDigit(number);
+Console.WriteLine($"Двузначное число --> {resultDigit}");
+
