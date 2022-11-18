be926bfe7aa761c7d8944a3884deb695b3575496
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-18 13:03:40 +0300

    task008 done

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -5,4 +5,18 @@
 // -4, 16 да
 // 8,9 нет
 
+Console.WriteLine("Проверка, является ли одно число квадратом другого");
 
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void Square (int num1, int num2)
+{
+    if (num2 == num1 * num1 || num1 == num2 * num2) Console.WriteLine ("да");
+    else Console.WriteLine("нет");
+}
+
+Square (number1, number2);
