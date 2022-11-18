7c27725065aa2ed7636ec08db38919f00e2944bc
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-18 11:00:43 +0300

    task011 added

---
+++ Task011/Program.cs
@@ -0,0 +1,17 @@
+﻿// По заданному номеру четверти
+// 
+
+Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Quarter (int number)
+{
+    if (number == 1) return "x > 0, y > 0";
+    if (number == 2) return "x > 0, y < 0";
+    if (number == 3) return "x < 0, y < 0";
+    if (number == 4) return "x > 0, y < 0";
+    return "Введено некорректное значение";
+}
+
+Console.WriteLine (Quarter (num));
+
