fb7a51aa13c8852a8940e417bdfff3ee53482e68
IrinaPenkina <nikolay_irina@mail.ru>
2022-12-01 19:17:37 +0300

    task024 split

---
+++ Task024B/Program.cs
@@ -0,0 +1,39 @@
+﻿// Преобразование десятичного числа в двоичное
+// с помощью массива
+
+
+int[] ConversionToBinary (int value)
+{
+    int num = value;
+    int digits = 0;
+    while (num > 0)
+    {
+        num /= 2;
+        digits ++;
+    }
+    int[] array = new int[digits];
+    for (int i = 0; i < digits; i++)
+    {
+        array [digits - i - 1] = value % 2;
+        value /= 2;
+    }
+    return array;
+}
+
+void PrintArray(int[] arr, string beginStr, string separatorElems, string endStr)
+{
+    Console.Write(beginStr);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}{separatorElems}");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(endStr);
+}
+
+Console.WriteLine("Задайте число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+int[] binaryNumber = ConversionToBinary(decimalNumber);
+PrintArray(binaryNumber, "", "", "");
+
---
+++ Task024C/Program.cs
@@ -0,0 +1,20 @@
+﻿/// Преобразование десятичного числа в двоичное
+// без массива
+
+string ConvertToBinary (int value)
+{
+    string bin = string.Empty;
+    while (value > 0)
+    {
+        bin = value % 2 + bin;
+        value /= 2;
+    }
+    return bin;
+}
+
+
+
+Console.WriteLine("Задайте число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+string binaryNumber = ConvertToBinary(decimalNumber);
+Console.WriteLine($"{binaryNumber}");
